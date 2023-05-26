a0ab36e7eae726d79e3b4dea5ef3b13727d5f8be
akashmondal2000 <mondalakash494@gmail.com>
2023-05-27 02:54:40 +0530

    add Form,Input,InputNumber on manageProductsAddEditModule

--- src/components/ManageProductsAddEditModal/index.jsx
+++ src/components/ManageProductsAddEditModal/index.jsx
@@ -1,9 +1,11 @@
-import { Modal, Button, Form, Input,InputNumber } from 'antd';
+import { Modal, Button, Form, Input,InputNumber, message } from 'antd';
 
 const ManageProductsAddEditForm =({
     isEdit = false,
     isVisible = false,
     onCancel = ()=>{},
+    selectedProduct = null,
+    onSubmit = () =>{}
 })=>{
 
     return(
@@ -13,11 +15,66 @@ const ManageProductsAddEditForm =({
             onCancel={onCancel}
             footer={null}
         >
+        <Form 
+            name="productAddEditForm"
+            onFinish={(val)=>{
+                console.log("Submitted Form data", val);
+                onsubmit(val);
+            }}
+
+            onFinishFailed={(value)=>{}}
+            autoComplete="off"
+            initialValues={
+                selectedProduct?
+                {
+                    name:selectedProduct.name,
+                    price:selectedProduct.price
+                }
+                :null
+            }            
+        >
+
+        {/*this is for product name */}
+        <Form.Item
+        label="Product Name"
+        name= "name"
+        rules={[
+            {
+                required:true,
+                message:"Please input your product name"
+            }
+        ]}
+        >
+        <Input/>
+        </Form.Item>
+
+        {/*This is for item price */}
+
+        <Form.Item
+        label="Price"
+        name="Price"
+        rules={[
+            {
+                required:true,
+                message :"Please input product price"
+            }
+        ]}
+        >
+        <InputNumber style={{width : "418px"}} prefix={"$ "}/>
+        </Form.Item>
+
+        <Form.Item
+        wrapperCol={{
+        offset: 10,
+        span: 16,
+      }}
+        >
 
             <Button type="primary" htmlType="submit">
             Submit
             </Button>
-
+            </Form.Item>
+        </Form>    
         </Modal>
     );
 }
