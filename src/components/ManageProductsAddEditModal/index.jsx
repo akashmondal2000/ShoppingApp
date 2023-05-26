f648b9cf2c0c41309bd3a0f54eb2a00c8e8477ff
akashmondal2000 <mondalakash494@gmail.com>
2023-05-27 01:37:29 +0530

    create component and add a module

---
+++ src/components/ManageProductsAddEditModal/index.jsx
@@ -0,0 +1,17 @@
+import { Modal, Button } from 'antd';
+
+const ManageProductsAddEditForm =({})=>{
+
+    return(
+        <Modal>
+
+            <Button>
+
+            </Button>
+
+        </Modal>
+    );
+}
+
+
+export default ManageProductsAddEditForm;
